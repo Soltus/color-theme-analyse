6022422aaa84c2f1404f1a3b8a53b73107b45c52
Soltus <liaoshanyi@qq.com>
2021-07-22 01:00:07 +0800

    pypi分支试上传

--- setup.py
+++ setup.py
@@ -75,25 +75,25 @@ setuptools.setup(
     scripts=['src/MMCQsc.cmd'],
     license="MIT",
     platforms=['Windows'],
-    python_requires=">=3.8",
+    python_requires=">=3.8.0",
     # 关于 install_requires， 有以下五种常用的表示方法：(指定包名是必须的，而版本控制与可选依赖，则是高级形式。 这不仅仅是install_requires的形式，而是对setup.py的所有require都适用)
-    # 'argparse'，只包含包名。 这种形式只检查包的存在性，不检查版本。 方便，但不利于控制风险。
+    # 'argparse'，只包含包名。 这种形式只检查包的存在性，不检查版本。 方便，但不利于控制风险，更新时还会遇到依赖冲突问题。
     # 'setuptools==38.2.4' 指定版本。确保了开发、测试与部署的版本一致，不会出现意外。 缺点是不利于更新，每次更新都需要改动代码。
     # 'docutils >= 0.3'，这是比较常用的形式。 当对某个库比较信任时，这种形式可以自动保持版本为最新。
     # 'Django >= 1.11, != 1.11.1, <= 2'，这是比较复杂的形式。保证了Django的大版本在1.11和2之间，也即1.11.x；并且，排除了已知有问题的版本1.11.1（仅举例）。 对于一些大型、复杂的库，这种形式是最合适的。
     # 'requests[security, socks] >= 2.18.4'，这是包含了额外的可选依赖的形式。 正常安装requests会自动安装它的install_requires中指定的依赖，而不会安装security和socks这两组依赖（这两组依赖是定义在它的extras_require中）。
     install_requires=[
-        'easygui',
-        'opencv-python',
-        'numpy',
-        'pillow',
-        'rich',
+        'easygui>=0.1',
+        'opencv-python>=4.0',
+        'numpy>=1.21',
+        'pillow>=8.3',
+        'rich>=0.1',
     ],
     # install_requires 在安装模块时会自动安装依赖包
     # extras_require 仅表示该模块依赖这些包，但不是必须的，需要用户手动安装。被依赖使用时，可以用类似'requests[download, advanced]'的形式来指定
     # extras_require 需要一个 dict ，其中按（自定义的）功能名称进行分组，每组一个 list
     extras_require={
-        'download':['pipx'],
+        'download':['pipx>=0.1'],
         'advanced':['pyinstaller>=4.3'],
     },
     # dependency_links 已被弃用，因此使用 testPyPi 测试时，一些依赖项会无法下载（ https://test-files.pythonhosted.org 上没有），需要提前安装好依赖，然后 pip 后面加参数 --no-deps 无依赖下载
