1b39ff60fc48110692ab2d68aa8779de75e6d3d5
Soltus <liaoshanyi@qq.com>
2021-08-28 01:43:38 +0800

    no message

--- src/MMCQsc/scp/main.py
+++ src/MMCQsc/scp/main.py
@@ -139,6 +139,8 @@ def mainFunc():
 
 
 if __name__ == '__main__':
+    import multiprocessing as mp
+    mp.set_start_method('spawn')
     try:
         result = mainFunc()
     except BaseException as e:
