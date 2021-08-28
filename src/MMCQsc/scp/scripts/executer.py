1acc385b554b6f7ad5d14cb26ddd27669c150f65
Soltus <liaoshanyi@qq.com>
2021-08-28 13:23:00 +0800

    1.2

--- src/MMCQsc/scp/scripts/executer.py
+++ src/MMCQsc/scp/scripts/executer.py
@@ -152,8 +152,8 @@ def procompress(files, root):
     for f in files:
         if os.path.splitext(f)[1].lower() in ['.jpg', 'jpeg', '.png']:
             ipl = ipl + 1
-            new_file_path = r'%s\%s_%s_%s%s' % (
-                os.path.join(SRC_DIR, "prepare"), 'img', ipl, str(int(time.time()*10000)), os.path.splitext(f)[1])
+            new_file_path = r'%s%s_%s_%s%s' % (
+                os.path.join(SRC_DIR, 'prepare', 'img'), ipl, str(int(time.time()*10000)), os.path.splitext(f)[1])
             new_file_path = os.path.abspath(new_file_path)
             shutil.copy2(os.path.join(root, f), new_file_path)
             ss.append(new_file_path)
