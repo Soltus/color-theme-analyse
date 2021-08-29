0b9ccc86fe2247b07126bbd3cf872a1a720cc327
Soltus <liaoshanyi@qq.com>
2021-08-29 19:30:39 +0800

    setup.py auto commit

--- setup.py
+++ setup.py
@@ -109,6 +109,9 @@ class GVC(distutils.cmd.Command):
         """命令运行时的操作."""
         print("======= command is running =======")
         self.default_nv()
+        global CLEAN_TAG
+        if CLEAN_TAG == True:
+            return 0
         command = [f'{sys.executable}']
         args = ['gitup.py','--version',self.version,'--workdir',os.getcwd(),'--commit']
         if self.version:
