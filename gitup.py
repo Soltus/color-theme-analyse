51ca641535989ed2df94347c21bec9949eef66fb
Soltus <liaoshanyi@qq.com>
2021-08-29 19:39:29 +0800

    setup.py auto commit

--- gitup.py
+++ gitup.py
@@ -18,6 +18,8 @@ def git_v_tag(v,c,cwd):
     请确保命令行能够正确使用 Git 命令。
     应当注意，将构建时动态写入的文件从 Git 中移除
     """
+    if c == '0.0.0':
+        return 0
     args = shlex.split(f"git add .")
     repo = Popen(args, bufsize=0, executable=None, close_fds=False, shell=True, env=None, startupinfo=None, creationflags=0, universal_newlines=True, stdout=PIPE, cwd=cwd)
     repo.wait()
@@ -27,10 +29,9 @@ def git_v_tag(v,c,cwd):
         repo = Popen(args, bufsize=0, executable=None, close_fds=False, shell=True, env=None, startupinfo=None, creationflags=0, universal_newlines=True, stdout=PIPE, cwd=cwd)
         repo.wait()
     # 打标签应当在提交之后，生成干净的无本地标识符的包
-    if v == '0.0.0':
-        args = shlex.split(f"git tag {v}")
-        repo = Popen(args, bufsize=0, executable=None, close_fds=False, shell=True, env=None, startupinfo=None, creationflags=0, universal_newlines=True, stdout=PIPE, cwd=cwd)
-        repo.wait()
+    args = shlex.split(f"git tag {v}")
+    repo = Popen(args, bufsize=0, executable=None, close_fds=False, shell=True, env=None, startupinfo=None, creationflags=0, universal_newlines=True, stdout=PIPE, cwd=cwd)
+    repo.wait()
 
 
 parser = argparse.ArgumentParser(prog='gitup',formatter_class=argparse.RawDescriptionHelpFormatter, description='功能介绍', epilog='基于 MMCQ 对图片进行色彩主题分析，采用图片压缩和多进程来加速批量分析速度。\n\n https://gitee.com/hi-windom/color-theme-analyse  \n \n ')
