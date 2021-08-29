ca68ea318dd666b94f3844b6d1ffbeaa888bc4d8
Soltus <liaoshanyi@qq.com>
2021-08-29 15:12:49 +0800

    setup.py auto commit

--- gitup.py
+++ gitup.py
@@ -4,7 +4,7 @@ import sys
 class gitupC:
     pass
 
-parser = argparse.ArgumentParser(prog='color-theme-analyse',formatter_class=argparse.RawDescriptionHelpFormatter, description='功能介绍', epilog='基于 MMCQ 对图片进行色彩主题分析，采用图片压缩和多进程来加速批量分析速度。\n\n https://gitee.com/hi-windom/color-theme-analyse  \n \n ')
+parser = argparse.ArgumentParser(prog='gitup',formatter_class=argparse.RawDescriptionHelpFormatter, description='功能介绍', epilog='基于 MMCQ 对图片进行色彩主题分析，采用图片压缩和多进程来加速批量分析速度。\n\n https://gitee.com/hi-windom/color-theme-analyse  \n \n ')
 parser.add_argument('--version',default=None, help='define version')
 gitup = gitupC()
 args = parser.parse_args(args=sys.argv,namespace=gitup)
