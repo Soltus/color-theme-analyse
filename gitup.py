63a9a5396aadafa84df1cc9d0773d8ccfe1b4b57
Soltus <liaoshanyi@qq.com>
2021-08-29 16:12:28 +0800

    setup.py auto commit

--- gitup.py
+++ gitup.py
@@ -12,7 +12,7 @@ class Cgitup(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         print('\n%r = %r' % (option_string,values))
         setattr(namespace, self.dest, values)
-        print(namespace)
+        # print(namespace)
 
 
 parser = argparse.ArgumentParser(prog='gitup',formatter_class=argparse.RawDescriptionHelpFormatter, description='功能介绍', epilog='基于 MMCQ 对图片进行色彩主题分析，采用图片压缩和多进程来加速批量分析速度。\n\n https://gitee.com/hi-windom/color-theme-analyse  \n \n ')
