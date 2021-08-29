52ac472bf02898db7571d02423f571627e00dfd3
Soltus <liaoshanyi@qq.com>
2021-08-29 15:33:26 +0800

    setup.py auto commit

--- gitup.py
+++ gitup.py
@@ -8,12 +8,16 @@ class Cgitup(argparse.Action):
         if nargs is not None:
             raise ValueError("nargs not allowed")
         super().__init__(option_strings, dest, **kwargs)
+
     def __call__(self, parser, namespace, values, option_string=None):
         print('%r %r %r' % (namespace, values, option_string))
         setattr(namespace, self.dest, values)
+        print('%r %r %r' % (namespace, values, option_string))
+
 parser = argparse.ArgumentParser(prog='gitup',formatter_class=argparse.RawDescriptionHelpFormatter, description='功能介绍', epilog='基于 MMCQ 对图片进行色彩主题分析，采用图片压缩和多进程来加速批量分析速度。\n\n https://gitee.com/hi-windom/color-theme-analyse  \n \n ')
-parser.add_argument('--version',default=None,action=Cgitup, help='define version')
+parser.add_argument('--version',default=None, action=Cgitup, help='define version')
 parser.add_argument('--commit',default= False, action=argparse.BooleanOptionalAction)
+parser.add_argument('--workdir',default=None, action=Cgitup, help='工作区')
 gitup = gitupC()
 args = parser.parse_args(args=sys.argv)
 # print(gitup.version)
