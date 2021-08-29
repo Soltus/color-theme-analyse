56f4f8129e9ff53af4fa4f0429d915249d5e1504
Soltus <liaoshanyi@qq.com>
2021-08-29 22:02:08 +0800

    setup.py auto commit

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ class GVC(distutils.cmd.Command):
         # 如果 stdout 参数是 PIPE，此属性是一个类似 open() 返回的可读流。从流中读取子进程提供的输出。
         # 如果 encoding 或 errors 参数被指定或者 universal_newlines 参数为 True，此流为文本流，否则为字节流。如果 stdout 参数非 PIPE，此属性为 None。
         vstr = result.stdout.read()
-        if not self.quiet:
+        if self.quiet == False:
             print(f'latest git tag: {vstr}')
             print(f'latest version: {my_v}')
         result.wait()
--- src/MMCQsc/version.py
+++ src/MMCQsc/version.py
@@ -1,2 +1,2 @@
-__version__ = "1.2.849.dev0"
-version = "1.2.849.dev0"
+__version__ = "1.2.849.dev1"
+version = "1.2.849.dev1"
