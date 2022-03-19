eabcb0d599e1c72194882525f8859340b75193d3
viviSoltus <liaoshanyi@qq.com>
2022-03-19 22:29:12 +0800

    setup.py auto commit

--- src/MMCQsc/scp/scripts/pipw.py
+++ src/MMCQsc/scp/scripts/pipw.py
@@ -1,5 +1,5 @@
-import os,sys
-
+import os,sys,shlex
+from subprocess import Popen
 
 def reinstallBase():
     '''
@@ -17,7 +17,12 @@ def reinstallMerge():
     '''
     仅用于调试
     '''
-    os.system("pip install color-theme-analyse[merge] -i https://mirrors.tencent.com/pypi/simple --force-reinstall")
+    python = os.path.abspath(sys.executable).replace('\\','/')
+    args = shlex.split(f"{python} -m pip install color-theme-analyse[merge] --force-reinstall -i https://mirrors.tencent.com/pypi/simple --extra-index-url https://mirrors.tencent.com/pypi/simple --compile --timeout 30 --exists-action b --only-binary :all:")
+    result = Popen(args, bufsize=0, executable=None, close_fds=False, shell=True, env=None, startupinfo=None, creationflags=0)
+    exit(100)
+    # result.wait()
+    # os.system("pip install color-theme-analyse[merge] -i https://mirrors.tencent.com/pypi/simple --force-reinstall")
 
 def uninstall_base():
     os.system("pip uninstall numpy -y")
--- src/MMCQsc/version.py
+++ src/MMCQsc/version.py
@@ -1,2 +1,2 @@
-__version__ = "1.54.2.dev0"
-version = "1.54.2.dev0"
+__version__ = "1.55.2.dev0"
+version = "1.55.2.dev0"
