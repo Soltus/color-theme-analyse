db1c1b271ca8f53eeaf2a3320c48afa9ab1cb6d0
viviSoltus <liaoshanyi@qq.com>
2022-03-19 22:41:27 +0800

    setup.py auto commit

--- src/MMCQsc/scp/scripts/pipw.py
+++ src/MMCQsc/scp/scripts/pipw.py
@@ -1,23 +1,25 @@
 import os,sys,shlex
 from subprocess import Popen
+from MMCQsc.version import version
+my_v = version
 
 def reinstallBase():
     '''
     仅用于调试
     '''
-    os.system("pip install color-theme-analyse[base] -i https://mirrors.tencent.com/pypi/simple --force-reinstall --user")
+    os.system(f"pip install color-theme-analyse[base]=={my_v} -i https://mirrors.tencent.com/pypi/simple --force-reinstall --user")
 
 def reinstallDev():
     '''
     仅用于调试
     '''
-    os.system("pip install color-theme-analyse[dev] -i https://mirrors.tencent.com/pypi/simple --force-reinstall --user")
+    os.system(f"pip install color-theme-analyse[dev]=={my_v} -i https://mirrors.tencent.com/pypi/simple --force-reinstall --user")
 
 def reinstallMerge():
     '''
     仅用于调试
     '''
-    os.system("pip install color-theme-analyse[merge] -i https://mirrors.tencent.com/pypi/simple --force-reinstall --user")
+    os.system(f"pip install color-theme-analyse[merge]=={my_v} -i https://mirrors.tencent.com/pypi/simple --force-reinstall --user")
 
 def uninstall_base():
     os.system("pip uninstall numpy -y")
--- src/MMCQsc/version.py
+++ src/MMCQsc/version.py
@@ -1,2 +1,2 @@
-__version__ = "1.57.2.dev0"
-version = "1.57.2.dev0"
+__version__ = "1.58.2.dev0"
+version = "1.58.2.dev0"
