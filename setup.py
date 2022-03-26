2c8ec6f3cfc0ce243ffd1873b7fa8956fe0e917b
viviSoltus <liaoshanyi@qq.com>
2022-03-26 22:55:03 +0800

    setup.py auto commit

--- setup.py
+++ setup.py
@@ -294,12 +294,20 @@ class GVC(distutils.cmd.Command):
 
 import setuptools.command.build_py
 class BuildPyCommand(setuptools.command.build_py.build_py):
-    """用法：python setup.py build_py"""
+    """接管 python setup.py build_py"""
 
     def run(self):
         setuptools.command.build_py.build_py.run(self)
 
+import setuptools.command.sdist
+class SdistCommand(setuptools.command.sdist.sdist):
+    """接管 python setup.py sdist"""
 
+    def run(self):
+        try:
+            setuptools.command.sdist.sdist.run(self)
+        except ModuleNotFoundError as e:
+            raise ModuleNotFoundError("猜测：six 模块损坏\n建议：使用与 sdist 命令绑定的Python执行 pip install six --force-reinstall 后重试")
 
 
 
@@ -313,7 +321,7 @@ print('开始执行，若长时间无响应，请检查是否有误\n')
 
 setuptools.setup(
     name="color-theme-analyse", # 在 PyPI 上搜索的项目名称
-    cmdclass={'GVC': GVC,'build_py': BuildPyCommand,}, # ---> 直接运行/调试本文件而不附加合适的参数时，这里报错是正常情况！请跳转到本文件的头部查看合适的指令 <---
+    cmdclass={'GVC': GVC,'build_py': BuildPyCommand,'sdist': SdistCommand}, # ---> 直接运行/调试本文件而不附加合适的参数时，这里报错是正常情况！请跳转到本文件的头部查看合适的指令 <---
     setup_requires=[], # 指定运行 setup.py 文件本身所依赖的包 , 国内由于众所周知的原因会假死，因此留空即可
     use_scm_version=True, # .gitignore 应与 setup.py 在同一文件夹 更多信息参考 https://pypi.org/project/setuptools-scm/
     # version='1.1.1', # 默认的手动指定版本
